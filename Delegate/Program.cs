644ae93689f846ca9473c2abb865597fbeae2c1c
JinKazama2019 <alteracii@gmail.com>
2020-11-23 13:16:49 +0200

    Add project files.

---
+++ Delegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    //объявление глобального делегата
+    public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
+    class Program
+    {
+        //объявление делегата внутри класса
+        public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
