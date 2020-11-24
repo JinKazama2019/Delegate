71b401480c4e77916dd963f10978cb0e801c8f44
JinKazama2019 <alteracii@gmail.com>
2020-11-24 17:53:39 +0200

    Add methods and variable of delegate

--- Delegate/Program.cs
+++ Delegate/Program.cs
@@ -7,14 +7,36 @@ using System.Threading.Tasks;
 namespace Delegate
 {
     //объявление глобального делегата
-    public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
+    // public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
+    public delegate void MyDelegate();
     class Program
     {
         //объявление делегата внутри класса
-        public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
-        static void Main(string[] args)
+       // public delegate Тип_возвращаемого_значения имя_делегата(тип_аргумента аргумент);
+         void Main(string[] args)
         {
-            Console.Write("Hello World");
+            MyDelegate myDelegate = Method1;// определили переменную делегата и присваиваем имя метода
         }
+
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        public int Method2()
+        {
+            Console.WriteLine("Method2");
+            return 0;
+        } 
+        public void Method3(int i)
+        {
+            Console.WriteLine("Method4");
+        }
+        public void Method4()
+        {
+            Console.WriteLine("Method4");
+        }
+
+
     }
 }
